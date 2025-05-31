2cc2aeef8d26fae4573d8efc18226cd387f4d637
Ibai <ibaiminiaturas@gmail.com>
2025-05-31 20:07:35 +0200

    Added video table, I already see I have to create the video-tags table, but as I said, I go step by step

--- Level2/youtube.sql
+++ Level2/youtube.sql
@@ -12,3 +12,19 @@ CREATE TABLE youtube_user (
     country VARCHAR(60) NOT NULL,
     zip_code VARCHAR(20) NOT NULL	
     );
+
+CREATE TABLE video (
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    user_id INT UNSIGNED NOT NULL,
+    publishing_date DATETIME NOT NULL,
+	title VARCHAR(100) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    file_name VARCHAR(100) NOT NULL,
+    size DOUBLE UNSIGNED NOT NULL,
+    duration DOUBLE UNSIGNED NOT NULL,
+	thumbnail MEDIUMBLOB NOT NULL,
+    reproductions BIGINT UNSIGNED , 
+    likes BIGINT UNSIGNED , 
+    dislikes BIGINT UNSIGNED,
+    state ENUM ("Public", "Hidden" , "Private") NOT NULL
+    );
