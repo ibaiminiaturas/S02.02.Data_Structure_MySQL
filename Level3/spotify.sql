cba8d71ceb5772ea3f6e6857b71abebff8e7a431
Ibai <ibaiminiaturas@gmail.com>
2025-06-01 16:29:40 +0200

    Added the playlist table and the playlist->song table with its relations. But Next I have to create the songs table

--- Level3/spotify.sql
+++ Level3/spotify.sql
@@ -27,7 +27,6 @@ CREATE TABLE subscription(
 CREATE TABLE credit_card(
 	number VARCHAR(19) NOT NULL UNIQUE PRIMARY KEY, -- In this case if a credit card  changes is really a new creadit card so ...
     user_id INT UNSIGNED NOT NULL,
-    PRIMARY KEY (number, user_id),
     expiration_month VARCHAR(2) NOT NULL,
     expiration_year VARCHAR(4) NOT NULL,
     cvs VARCHAR(3) NOT NULL,
@@ -52,5 +51,28 @@ CREATE TABLE subscription_payment(
     
 );
 
+CREATE TABLE playlist (
+	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    playlist_name VARCHAR(60) NOT NULL,
+	creation_date DATETIME NOT NULL,
+    songs_number INT UNSIGNED, 
+    user_id INT UNSIGNED NOT NULL,
+    is_deleted TINYINT DEFAULT 0, -- not real booleans in mysql, weird...
+    deletion_date DATETIME NULL,
+    is_shared TINYINT DEFAULT 0, -- not real booleans in mysql, weird...
+	CONSTRAINT fk_playlist_user_id FOREIGN KEY (user_id) REFERENCES spotify_user(id)
+);
+
+
+-- Here I am supossing that you can not add a song to a playlist more than once, no? it would be a bit stupid I think.
+CREATE TABLE playlist_songs(
+	playlist_id INT UNSIGNED NOT NULL,
+    song_id INT UNSIGNED NOT NULL,
+    user_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (playlist_id, song_id),
+    adding_date DATETIME NOT NULL,
+    CONSTRAINT fk_splaylist_songs_user_id FOREIGN KEY (user_id) REFERENCES spotify_user(id),
+    CONSTRAINT fk_playlist_songs_playlist_id FOREIGN KEY (playlist_id) REFERENCES playlist(id)
+);
 
 
